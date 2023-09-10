7b24046bcb1a5585f167b031fafdc0c6d533dbbb
nicoly_assalin <144288307+niczada@users.noreply.github.com>
2023-09-10 19:35:21 -0300

    Update questao 1.cpp

--- questao 1.cpp
+++ questao 1.cpp
@@ -1,19 +1,1 @@
-// questão 1
-#include <stdio.h>
-int main(){
-	
-	float n1;
-	float n2;
-	float m
-	
-printf("insira sua primeira nota: ");
-scanf("%f, &n1");
 
-printf("insira sua segunda nota: ")
-scanf("%f", &n2);
-
-m = (n1 + n2) /2 ;
-
-printf("o resultado da media é: %f" , m);
-return 0;	
-}
